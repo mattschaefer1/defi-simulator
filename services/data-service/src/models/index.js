5a77405bfdfbb663c053bcf0adab671e8f0540a0
Matt Schaefer <matthewschaefer47@gmail.com>
2025-02-26 18:36:52 -0700

    Define sequelize associations for models

--- services/data-service/src/models/index.js
+++ services/data-service/src/models/index.js
@@ -31,6 +31,17 @@ const models = {
   ETHStakingHistorical: ETHStakingHistorical.init(sequelize)
 };
 
-// TODO: Define associations
+// Define associations
+models.Pool.belongsTo(models.Token, { as: 'token0', foreignKey: 'token0_symbol' });
+models.Token.hasMany(models.Pool, { as: 'poolsAsToken0', foreignKey: 'token0_symbol' });
+
+models.Pool.belongsTo(models.Token, { as: 'token1', foreignKey: 'token1_symbol' });
+models.Token.hasMany(models.Pool, { as: 'poolsAsToken1', foreignKey: 'token1_symbol' });
+
+models.TokenPrice.belongsTo(models.Token, { foreignKey: 'token_symbol' });
+models.Token.hasMany(models.TokenPrice, { foreignKey: 'token_symbol' });
+
+models.LPHistorical.belongsTo(models.Pool, { foreignKey: 'pool_address' });
+models.Pool.hasMany(models.LPHistorical, { foreignKey: 'pool_address' });
 
 export { sequelize, models };
