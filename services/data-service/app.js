79a60c38e935fbf673dfe008765759aad94caac7
Matt Schaefer <matthewschaefer47@gmail.com>
2025-03-25 17:14:16 -0600

    Move data fetching and saving to dedicated handler

--- services/data-service/app.js
+++ services/data-service/app.js
@@ -5,9 +5,7 @@ import { fileURLToPath } from 'url';
 import morgan from 'morgan';
 import dataRoutes from './src/routes/dataRoutes.js';
 import { sequelize, models } from './src/models/index.js';
-import { fetchPoolData, fetchPriceData, fetchUniswapPoolData } from './src/services/dataFetcher.js';
-import { trimData, removeDuplicateTimestamps, findMissingDates, fillMissingDates, formatLiquidityPoolData, addSymbolToPriceData, processPoolDataResponse, processPriceDataResponse, processUniswapPoolDataResponse } from './src/processors/dataProcessor.js';
-import { saveStakingData, saveTokenPriceData, saveLiquidityPoolData } from './src/savers/dataSaver.js';
+import dataHandler from './src/handlers/dataHandler.js';
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
@@ -34,50 +32,8 @@ app.locals.models = models;
 // Routes
 app.use('/api/data', dataRoutes);
 
-// Fetch data
-const apyTvlData = await fetchPoolData();
-const priceData = await fetchPriceData(365);
-const uniswapPoolsData = await fetchUniswapPoolData(365);
-
-// Process data
-const [processedApyData, processedTvlData] = processPoolDataResponse(apyTvlData);
-const processedPriceData = processPriceDataResponse(priceData);
-const processedUniswapPoolsData = processUniswapPoolDataResponse(uniswapPoolsData);
-
-const cleanApyData = removeDuplicateTimestamps(processedApyData);
-const cleanTvlData = removeDuplicateTimestamps(processedTvlData);
-const cleanPriceData = removeDuplicateTimestamps(processedPriceData);
-const cleanUniswapPoolsData = removeDuplicateTimestamps(processedUniswapPoolsData);
-
-const trimmedApyData = trimData(cleanApyData);
-const trimmedTvlData = trimData(cleanTvlData);
-const trimmedPriceData = trimData(cleanPriceData);
-const trimmedUniswapPoolsData = trimData(cleanUniswapPoolsData);
-
-const missingApyDates = findMissingDates(trimmedApyData);
-const missingTvlDates = findMissingDates(trimmedTvlData);
-const missingPriceDates = findMissingDates(trimmedPriceData);
-const missingUniswapDates = findMissingDates(trimmedUniswapPoolsData);
-
-const filledApyData = Object.keys(missingApyDates).length > 0
-  ? trimData(fillMissingDates(trimmedApyData, missingApyDates))
-  : trimmedApyData;
-const filledTvlData = Object.keys(missingTvlDates).length > 0
-  ? trimData(fillMissingDates(trimmedTvlData, missingTvlDates))
-  : trimmedTvlData;
-const filledPriceData = Object.keys(missingPriceDates).length > 0
-  ? trimData(fillMissingDates(trimmedPriceData, missingPriceDates))
-  : trimmedPriceData;
-const filledUniswapPoolsData = Object.keys(missingUniswapDates).length > 0
-  ? trimData(fillMissingDates(trimmedUniswapPoolsData, missingUniswapDates))
-  : trimmedUniswapPoolsData;
-
-const liquidityPoolData = formatLiquidityPoolData(filledTvlData, filledUniswapPoolsData);
-const tokenPriceData = addSymbolToPriceData(filledPriceData);
-
-await saveStakingData(filledApyData, app);
-await saveTokenPriceData(tokenPriceData, app);
-await saveLiquidityPoolData(liquidityPoolData, app);
+// Fetch and save data to database
+await dataHandler(app);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
---
+++ services/data-service/src/handlers/dataHandler.js
@@ -0,0 +1,66 @@
+import {
+  fetchPoolData,
+  fetchPriceData,
+  fetchUniswapPoolData
+} from '../services/dataFetcher.js';
+import {
+  saveStakingData,
+  saveTokenPriceData,
+  saveLiquidityPoolData
+} from '../savers/dataSaver.js';
+import {
+  trimData,
+  removeDuplicateTimestamps,
+  findMissingDates,
+  fillMissingDates,
+  formatLiquidityPoolData,
+  addSymbolToPriceData,
+  processPoolDataResponse,
+  processPriceDataResponse,
+  processUniswapPoolDataResponse
+} from '../processors/dataProcessor.js';
+
+export default async function dataHandler(app) {
+  const apyTvlData = await fetchPoolData();
+  const priceData = await fetchPriceData(365);
+  const uniswapPoolsData = await fetchUniswapPoolData(365);
+
+  const [processedApyData, processedTvlData] = processPoolDataResponse(apyTvlData);
+  const processedPriceData = processPriceDataResponse(priceData);
+  const processedUniswapPoolsData = processUniswapPoolDataResponse(uniswapPoolsData);
+
+  const cleanApyData = removeDuplicateTimestamps(processedApyData);
+  const cleanTvlData = removeDuplicateTimestamps(processedTvlData);
+  const cleanPriceData = removeDuplicateTimestamps(processedPriceData);
+  const cleanUniswapPoolsData = removeDuplicateTimestamps(processedUniswapPoolsData);
+
+  const trimmedApyData = trimData(cleanApyData);
+  const trimmedTvlData = trimData(cleanTvlData);
+  const trimmedPriceData = trimData(cleanPriceData);
+  const trimmedUniswapPoolsData = trimData(cleanUniswapPoolsData);
+
+  const missingApyDates = findMissingDates(trimmedApyData);
+  const missingTvlDates = findMissingDates(trimmedTvlData);
+  const missingPriceDates = findMissingDates(trimmedPriceData);
+  const missingUniswapDates = findMissingDates(trimmedUniswapPoolsData);
+
+  const filledApyData = Object.keys(missingApyDates).length > 0
+    ? trimData(fillMissingDates(trimmedApyData, missingApyDates))
+    : trimmedApyData;
+  const filledTvlData = Object.keys(missingTvlDates).length > 0
+    ? trimData(fillMissingDates(trimmedTvlData, missingTvlDates))
+    : trimmedTvlData;
+  const filledPriceData = Object.keys(missingPriceDates).length > 0
+    ? trimData(fillMissingDates(trimmedPriceData, missingPriceDates))
+    : trimmedPriceData;
+  const filledUniswapPoolsData = Object.keys(missingUniswapDates).length > 0
+    ? trimData(fillMissingDates(trimmedUniswapPoolsData, missingUniswapDates))
+    : trimmedUniswapPoolsData;
+
+  const liquidityPoolData = formatLiquidityPoolData(filledTvlData, filledUniswapPoolsData);
+  const tokenPriceData = addSymbolToPriceData(filledPriceData);
+
+  await saveStakingData(filledApyData, app);
+  await saveTokenPriceData(tokenPriceData, app);
+  await saveLiquidityPoolData(liquidityPoolData, app);  
+}
