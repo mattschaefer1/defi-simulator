7c20b42d3e1d21769dd4854ed2295e3905c8e452
Matt Schaefer <matthewschaefer47@gmail.com>
2025-04-04 13:28:33 -0600

    Refactor processing functions - readability, consistency, and formatting

--- services/data-service/src/config/pools.js
+++ services/data-service/src/config/pools.js
@@ -1,4 +1,4 @@
-const poolIds = {
+export const poolIds = {
   lidoEth: '747c1d2a-c668-4682-b9f9-296708a3dd90',
   wethUsdc: '665dc8bc-c79d-4800-97f7-304bf368e547',
   wbtcUsdc: 'bbecbf69-a4f7-43e3-8b72-de180d106e2c',
@@ -6,18 +6,16 @@ const poolIds = {
   daiUsdc: '1193ef25-862b-43c1-a545-91bbb9678d30',
 };
 
-const tokenAddresses = {
+export const tokenAddresses = {
   weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
   wbtc: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
   dai: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
   usdc: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
 };
 
-const poolAddresses = {
+export const poolAddresses = {
   wethUsdc: '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640',
   wbtcUsdc: '0x99ac8ca7087fa4a2a1fb6357269965a2014abc35',
   wbtcWeth: '0xcbcdf9626bc03e24f779434178a73a0b4bad62ed',
   daiUsdc: '0x5777d92f208679db4b9778590fa3cab3ac9e2168',
 };
-
-export { poolIds, tokenAddresses, poolAddresses };
